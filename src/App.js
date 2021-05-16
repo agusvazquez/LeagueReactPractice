ef53bc529fd9e05835ce9d411852438be0a7d968
Agustin Alejandro Vazquez Ballan <aguss.vazquez@gmail.com>
2021-05-16 01:13:08 -0300

    Fixed the background when the list is shorter than the screen.

--- src/App.js
+++ src/App.js
@@ -8,19 +8,29 @@ import Header from "./components/header/header.component";
 function App() {
   return (
     <AppContainer>
-      <Router>
-        <Route path="/" component={Header} />
-        <Route exact path="/" component={ChampionList} />
-        <Route exact path="/champions" component={ChampionList} />
-        <Route exact path="/champions/:championId" component={ChampionDetail} />
-      </Router>
+      <AppContainerWithPadding>
+        <Router>
+          <Route path="/" component={Header} />
+          <Route exact path="/" component={ChampionList} />
+          <Route exact path="/champions" component={ChampionList} />
+          <Route
+            exact
+            path="/champions/:championId"
+            component={ChampionDetail}
+          />
+        </Router>
+      </AppContainerWithPadding>
     </AppContainer>
   );
 }
 
 const AppContainer = styled.div`
-  padding: 20px;
+  min-height: 100vh;
   background-color: black;
+`;
+
+const AppContainerWithPadding = styled.div`
+  padding: 20px;
 `;
 
 export default App;
